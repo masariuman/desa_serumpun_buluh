bbc62624d3abfbb67d6415fe625aed6d9269cb8c
masariuman <masariuman@gmail.com>
2016-12-25 10:11:41 +0700

    remove media textarea artikel

--- resources/views/admin/artikel/form.blade.php
+++ resources/views/admin/artikel/form.blade.php
@@ -56,10 +56,10 @@
     plugins: [
       "advlist autolink lists link image charmap print preview hr anchor pagebreak table",
       "searcheplace wordcount visualblocks visualchars code fullscreen",
-      "insertdatetime media nonbreaking save table contextmenu directionality",
+      "insertdatetime nonbreaking save table contextmenu directionality",
       "emoticons template paste textcolor colorpicker textpattern"
     ],
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table link image media",
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | table link image",
     relative_urls: false,
     file_browser_callback : function(field_name, url, type, win) {
       var x = window.innerWidth || document.documentElement.clienWidth || document.getElementsByTagName('body')[0].clientWidth;
